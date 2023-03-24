3269f88f0b050e87a814108e00241c42216299c3
+ <+>
2023-03-24 08:00:05 +0300

    completed phase1 tasklister mini project

--- src/index.js
+++ src/index.js
@@ -1,3 +1,16 @@
 document.addEventListener("DOMContentLoaded", () => {
   // your code here
+  const form = document.getElementById("create-task-form");
+  const taskList = document.getElementById("tasks");
+
+  form.addEventListener("submit", (tasks) => {
+    tasks.preventDefault();
+    const enteredTask = document.createElement("li");
+    const taskEntry = document.getElementById("new-task-description");
+
+    enteredTask.innerText = taskEntry.value;
+    taskList.appendChild(enteredTask);
+    taskEntry.value = "";
+  });
 });
+
